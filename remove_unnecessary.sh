e9ceaef1d5db4b3058353464489e1983e25296b2
Nipun Pruthi <nipun.iitrlv@gmail.com>
2018-01-07 18:11:32 +0530

    unnecessary_removed

--- remove_unnecessary.sh
+++ remove_unnecessary.sh
@@ -1,3 +1,3 @@
 #! /use/bin/env bash
-find . \(  -name "*.aux" -o  -name "*.log" -o  -name "*~" \) -type f -delete 
+find . \(  -name "*.aux" -o  -name "*.log" -o  -name "*~" -l -name "*.jou" \) -type f -delete 
 
